02c0066e6b27c73c9a0183bea5c483dcd97a4ac4
Lucas Mendes Madruga <lucas.madruga@kymo.it>
2021-12-19 22:19:45 -0300

    Estilização parcial tela home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
+import { AiOutlineCalendar } from 'react-icons/ai';
+import { FiUser } from 'react-icons/fi';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { useState } from 'react';
 
 interface Post {
   uid?: string;
@@ -24,9 +27,74 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  const [isLoadingMorePosts, setIsLoadingMorePosts] = useState<boolean>(false);
+
+  async function handleLoadPostsClick() {
+    setIsLoadingMorePosts(true);
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    setIsLoadingMorePosts(false);
+  }
+
+  return (
+    <main className={styles.container}>
+      <header>
+        <img src="/logo.svg" alt="logo" />
+      </header>
+      <section className={styles.postsContainer}>
+        <div className={styles.post}>
+          <h3>Como utilizar Hooks</h3>
+          <p>Pensando em sincronização em vez de ciclos.</p>
+
+          <footer>
+            <div>
+              <AiOutlineCalendar /> <span>15 Mar 2021</span>
+            </div>
+            <div>
+              <FiUser /> <span>Joseph Oliveira</span>
+            </div>
+          </footer>
+        </div>
+
+        <div className={styles.post}>
+          <h3>Como utilizar Hooks</h3>
+          <p>Pensando em sincronização em vez de ciclos.</p>
+
+          <footer>
+            <div>
+              <AiOutlineCalendar /> <span>15 Mar 2021</span>
+            </div>
+            <div>
+              <FiUser /> <span>Joseph Oliveira</span>
+            </div>
+          </footer>
+        </div>
+
+        <div className={styles.post}>
+          <h3>Como utilizar Hooks</h3>
+          <p>Pensando em sincronização em vez de ciclos.</p>
+
+          <footer>
+            <div>
+              <AiOutlineCalendar /> <span>15 Mar 2021</span>
+            </div>
+            <div>
+              <FiUser /> <span>Joseph Oliveira</span>
+            </div>
+          </footer>
+        </div>
+
+        <button
+          onClick={handleLoadPostsClick}
+          className={styles.loadMorePostsButton}
+          disabled={isLoadingMorePosts}
+        >
+          Carregar mais posts
+          </button>
+      </section>
+    </main>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
